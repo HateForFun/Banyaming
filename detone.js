941b5ffe9e0537c6647f9e1d4695a07cb07d9c14
HateForFun <92043297+HateForFun@users.noreply.github.com>
2023-07-07 14:06:46 +0300

    Add files via upload

---
+++ detone.js
@@ -0,0 +1,88 @@
+// Pinyin no tone, indexing purpose
+function deTone(input) 
+{
+  var inputString = String(input).toLowerCase();
+
+  const e_tones = ["e", "ē", "é", "ě", "è"];
+  const a_tones = ["a", "ā", "á", "ǎ", "à"];
+  const i_tones = ["i", "ī", "í", "ǐ", "ì"];
+  const o_tones = ["o", "ō", "ó", "ǒ", "ò"];
+  const u_tones = ["u", "ū", "ú", "ǔ", "ù"];
+  const v_tones = ["ü", "ǖ", "ǘ", "ǚ", "ǜ"];
+
+  for (let i = 0; i < e_tones.length; i++) 
+  {
+    inputString = inputString.replaceAll(e_tones[i], e_tones[0]);
+    inputString = inputString.replaceAll(a_tones[i], a_tones[0]);
+    inputString = inputString.replaceAll(i_tones[i], i_tones[0]);
+    inputString = inputString.replaceAll(o_tones[i], o_tones[0]);
+    inputString = inputString.replaceAll(u_tones[i], u_tones[0]);
+    inputString = inputString.replaceAll(v_tones[i], v_tones[0]);
+  }
+  return inputString;
+}
+
+function searchPinyin(query, input)
+{
+	out_options =[query,deTone(query)];
+	
+	for (let i = 0; i < out_options.length; i++) 
+	{
+		if (out_options[i].includes(input))
+		{
+			return true;
+		}
+	}
+	
+	return false;
+	
+}
+
+function highlight(text,cell) 
+{
+	var innerHTML = cell.textContent;
+	
+	var allLowCase = innerHTML.toLowerCase();
+	
+	var cellText;
+	
+	var index = allLowCase.indexOf(text.toLowerCase());
+	if (index >= 0) 
+	{ 
+		cellText = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+		cell.innerHTML = cellText;
+	}
+}
+
+function highlightPinyin(text,cell)
+{
+	var innerHTML = cell.textContent;
+	var filter = deTone(innerHTML);
+	
+	var index = filter.indexOf(text.toLowerCase());
+	if (index >= 0) 
+	{ 
+		innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+		cell.innerHTML = innerHTML;
+	}
+}
+
+function unhighlight(cell)
+{
+	cell.innerHTML = cell.textContent;
+	console.log(cell.innerHTML);
+}
+
+function unhighlightAll(row)
+{
+	for (j = 0; j < row.cells.length-1; j++)
+	{
+		//console.log(`j = ${j} , ${row.cells[j].textContent}`);
+		unhighlight(row.cells[j]);
+		//row.cells[j].getElementsByTagName('span').remove();
+	}
+}
+
+
+
+
---
+++ generate_table.js
@@ -0,0 +1,60 @@
+function generateTable(rows,cols) {
+  // creates a <table> element and a <tbody> element
+  const tbl = document.createElement("table");
+  const tblBody = document.createElement("tbody");
+
+  // creating all cells
+  for (let i = 0; i < rows; i++) {
+    // creates a table row
+    const row = document.createElement("tr");
+
+    for (let j = 0; j < cols; j++) {
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      const cell = document.createElement("td");
+      const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+	  cell.align ="center";
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+
+    // add the row to the end of the table body
+
+	tblBody.appendChild(row);
+	if (i !=0)
+	{
+		tblBody.appendChild(emptyRow(cols));
+	}
+  }
+
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+  document.body.appendChild(tbl);
+  // sets the border attribute of tbl to '2'
+  tbl.setAttribute("border", "2");
+  
+  tbl.width = "100%";
+  
+  return tbl;
+}
+
+function emptyRow(cols)
+{
+	const row = document.createElement("tr");
+	const cell = document.createElement("td");
+    const cellText = document.createTextNode("Empty");
+	cell.align ="left";
+	cell.appendChild(cellText);
+	cell.setAttribute('colspan', cols.toString());
+	row.appendChild(cell);
+	
+	return row;
+	
+}
+
+
+
+
+
